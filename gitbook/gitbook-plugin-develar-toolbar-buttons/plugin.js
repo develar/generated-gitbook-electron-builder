729cf8b9d63b6ed38c9966156f91001a46b2bfa0
develar <develar@gmail.com>
2017-09-01 13:38:50 +0200

    update

--- gitbook/gitbook-plugin-develar-toolbar-buttons/plugin.js
+++ gitbook/gitbook-plugin-develar-toolbar-buttons/plugin.js
@@ -1,13 +1,23 @@
-require(["gitbook"], (gitbook) => {
-  gitbook.events.bind('start', function (e, config) {
-    const githubURL = "/donate"
+"use strict"
 
-    gitbook.toolbar.createButton({
-      label: 'Donate',
-      position: 'right',
-      onClick: function () {
-        window.open(githubURL)
-      }
-    })
+require(["gitbook"], function (gitbook) {
+  function addBeforeHeader(element) {
+    jQuery(".book-header > h1").before(element)
+  }
+
+  function init() {
+    if (window.location.href.indexOf("/donate") > 0) {
+      return
+    }
+    //
+    // const elementString2 = `<a class="btn pull-right hidden-mobile" href="https://yarn.pm/electron-builder"><img src="https://img.shields.io/npm/v/electron-builder.svg?label=latest"/></a>`
+    // addBeforeHeader(elementString2)
+
+    const elementString = `<a class="btn pull-right hidden-mobile" aria-label="donate" href="/donate">Donate</a>`
+     addBeforeHeader(elementString)
+  }
+
+  gitbook.events.bind("page.change", () => {
+    init()
   })
 })
